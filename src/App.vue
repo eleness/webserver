4667f701dcf68bd173d957800d668e9dd33909a4
engir <work.home7@bk.ru>
2021-06-30 23:08:30 +0300

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <siteHeader/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import siteHeader from './components/siteHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    siteHeader
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/siteHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <a href="#"> <img src="https://i.ibb.co/MnQ2x6q/10.png" class="logoImg"> </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'siteHeader'
+}
+</script>
+
+
+<style scoped>
+.header {
+    padding: 1rem 25rem;
+}
+.header .logoImg{
+    max-width: 375px;
+    transition: 350ms;
+    transition-timing-function: cubic-bezier(0.68, -0.24, 0.05, 1.65);
+}
+.logoImg:hover {
+    transform: scale(1.04, 1.04);
+}
+</style>
